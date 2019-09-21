2699c28769b565949e410c5ddea62244b80e6010
Raul Olaya <rolaya@debian-contenta>
2019-09-21 07:46:01 -0400

    Updates to php-7.3-install.sh 1. Fetch package list from the new package source. 2. Echo all packages installed by script.

--- tools/php/php-7.3-install.sh
+++ tools/php/php-7.3-install.sh
@@ -19,6 +19,22 @@ check_package_requirement()
   fi
 }
 
+#==================================================================================================================
+# Install package(s).
+#==================================================================================================================
+install_package()
+{
+  # Check all parameters passed in command line (if any)
+  while [ -n "$1" ]; do
+
+    echo "${TEXT_VIEW_NORMAL_BLUE}Installing package: [$1]...${TEXT_VIEW_NORMAL}"
+    sudo apt-get install $1
+    shift
+
+  done
+}
+
+
 # php and wget must be installed
 check_package_requirement "wget"
 
@@ -26,7 +42,7 @@ check_package_requirement "wget"
 # ca-certificates: Certificate authorities (used for SSL connections authentication)
 # apt-transport-https: Enables the usage of 'deb https://foo distro main' lines
 # lsb-release: Linux Standard Base
-sudo apt-get install ca-certificates apt-transport-https lsb-release
+install_package ca-certificates apt-transport-https lsb-release
 
 # Get the GNU Privacy Guard key (from https://packages.sury.org/php) and
 # save it as /etc/apt/trusted.gpg.d/php.gpg
@@ -36,5 +52,8 @@ sudo wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gp
 # The apt applicaiton will use this source to provide php 7.3 packages, etc.
 sudo echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php7.3.list
 
+# Fetch the package list from the new source
+sudo apt update
+
 # Install main php7.3 and common extensions (and extensions required for drupal/contenta).
-sudo apt-get install php7.3 php7.3-cli php7.3-common php7.3-opcache php7.3-curl php7.3-mbstring php7.3-mysql php7.3-zip php7.3-xml php7.3-gd
+install_package php7.3 php7.3-cli php7.3-common php7.3-opcache php7.3-curl php7.3-mbstring php7.3-mysql php7.3-zip php7.3-xml php7.3-gd
