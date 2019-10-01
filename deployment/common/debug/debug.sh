e8c7f2b64527b8e944a2dc6536258e7da422cefe
Raul Olaya <raul.olaya@gmail.com>
2019-09-30 20:40:30 -0400

    Misc. "single install" contenta deployment modifications (in progress).
    1. Initial debug/log/echo message selection/configuration.

---
+++ deployment/common/debug/debug.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+#==================================================================================================================
+#==================================================================================================================
+init_debug_configuration()
+{
+  local var_reference=$1
+
+  # Here, we set the global debug mask
+  #local config_value=$((${gmask_debug_deployment} | ${gmask_debug_file_io}))
+  local config_value=$((${gmask_debug_deployment}))
+
+  # Set to true to get debug messages
+  local debug=false
+
+  if [ $debug = true ]; then
+    echo "Debug configuration: [$config_value]"
+  fi
+
+  # Update variable reference
+  eval $var_reference="'$config_value'" 
+}
