8af56abd0f83b9a00209500d89f520535ed1fb4b
Raul Olaya <raul.olaya@gmail.com>
2019-11-11 23:08:39 -0500

    Contenta infrastructure/deployment modifications
    1. Install guide updates.

--- deployment/main/deploy.sh
+++ deployment/main/deploy.sh
@@ -105,13 +105,18 @@ deployment_full_show_guide()
   guide_item="Contenta deployment "
   echo "${TEXT_FG_MAGENTA}${TEXT_SET_ATTR_ITALIC}$guide_item${TEXT_VIEW_RESET}"
   echo ""
-  guide_item="This process installs infrastructure required for Contenta"
+  guide_item="This process installs infrastructure required for Contenta deployment,"
   echo "${TEXT_FG_MAGENTA}${TEXT_SET_ATTR_ITALIC}$guide_item${TEXT_VIEW_RESET}"
-  guide_item="and gathers information from the user for deploying Contenta site"
+  guide_item="collects site configuration information from the user and deploys Contenta site"
+  echo "${TEXT_FG_MAGENTA}${TEXT_SET_ATTR_ITALIC}$guide_item${TEXT_VIEW_RESET}"
+  guide_item="using user defined information."
+  echo "${TEXT_FG_MAGENTA}${TEXT_SET_ATTR_ITALIC}$guide_item${TEXT_VIEW_RESET}"
+  echo ""
+  guide_item="sudo password is required."
   echo "${TEXT_FG_MAGENTA}${TEXT_SET_ATTR_ITALIC}$guide_item${TEXT_VIEW_RESET}"
   echo ""
 
-  guide_item="1. Installs AMP (Apache, MariaDB PHP) stack"
+	guide_item="1. Installs AMP (Apache, MariaDB PHP) stack"
   echo "${TEXT_FG_MAGENTA}${TEXT_SET_ATTR_ITALIC}$guide_item${TEXT_VIEW_RESET}"
 
   guide_item="   1.1. Apache2 (latest release version)"
@@ -132,10 +137,10 @@ deployment_full_show_guide()
   guide_item="3. Installs Python"
   echo "${TEXT_FG_MAGENTA}${TEXT_SET_ATTR_ITALIC}$guide_item${TEXT_VIEW_RESET}"  
 
-  guide_item="4. Creates MariaDB user with privileges to create users and databases"
+  guide_item="4. Creates MariaDB user with privileges to create contenta site user and database"
   echo "${TEXT_FG_MAGENTA}${TEXT_SET_ATTR_ITALIC}$guide_item${TEXT_VIEW_RESET}" 
 
-  guide_item="5. Collects Contenta site (to be created) information"
+  guide_item="5. Collects Contenta site information"
   echo "${TEXT_FG_MAGENTA}${TEXT_SET_ATTR_ITALIC}$guide_item${TEXT_VIEW_RESET}"  
 
   guide_item="   5.1. Contenta site name"
@@ -153,7 +158,7 @@ deployment_full_show_guide()
   guide_item="   5.5. Contenta site account email"
   echo "${TEXT_FG_MAGENTA}${TEXT_SET_ATTR_ITALIC}$guide_item${TEXT_VIEW_RESET}"
 
-  guide_item="   5.6. Contenta site MariaDB database name"
+  guide_item="   5.6. Contenta site database name"
   echo "${TEXT_FG_MAGENTA}${TEXT_SET_ATTR_ITALIC}$guide_item${TEXT_VIEW_RESET}"
 
   guide_item="   5.7. MariaDB user (see **)"
